7f556b68aa509532f20cade4488f991306453681
LucasIbrahinCardoso <lucasibraca@gmail.com>
2026-05-08 16:31:35 -0400

    Não sei, calma

---
+++ Frontend/atributos.js
@@ -0,0 +1,18 @@
+class Atributos{
+    constructor(dono, forca, velocidade, destreza, resistencia, inteligencia, sabedoria, vontade, carisma, influencia){
+        this.dono = dono
+        this.forc = forca;
+        this.vel = velocidade;
+        this.des = destreza;
+        this.res = resistencia;
+        this.int = inteligencia;
+        this.sab = sabedoria;
+        this.von = vontade;
+        this.car = carisma;
+        this.influ = influencia;
+    }
+
+
+
+    
+}
--- Frontend/script.js
+++ Frontend/script.js
@@ -1,14 +1,21 @@
 var inputNome = document.getElementById('nome-personagem');
-var o_nome = inputNome.value
 var spanNome = document.getElementById('nome-escolhido');
+var todos_atributos = getElementById('atributos-container')
 
 inputNome.addEventListener('keydown', function(event){
     if (event.key === 'Enter'){
+        var o_nome = inputNome.value
         spanNome.textContent = o_nome
+        
     }
 })
 
-var botao = document.getElementById('btn-criar');
-botao.addEventListener('click', function(){ 
-    
+
+
+
+
+
+var botao_criar = document.getElementById('btn-criar');
+botao_criar.addEventListener('click', function(){ 
+
 });
